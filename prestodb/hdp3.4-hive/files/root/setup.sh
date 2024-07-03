c8cb71ec80e08aa59581c0f827f35b762449cf7d
Andrew X <andrewx.git@gmail.com>
2024-07-03 16:13:18 -0700

    Fix hdp3.4 errors

--- prestodb/hdp3.4-hive/files/root/setup.sh
+++ prestodb/hdp3.4-hive/files/root/setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -ex
 
 mkdir -p /opt/hadoop/logs
-chmod 777 /opt/hadoop/logs
+chmod -R 777 /opt/hadoop
 
 # 2 start hdfs
 mkdir -p /var/log/hadoop-hdfs
 chown hdfs:hdfs /var/log/hadoop-hdfs
-su -c "hdfs namenode -format 2>&1 > /var/log/hadoop-hdfs/hadoop-hdfs-namenode.log" hdfs&
+
+su -c "echo 'N' | hdfs namenode -format" hdfs
+
+su -c "hdfs namenode  2>&1 > /var/log/hadoop-hdfs/hadoop-hdfs-namenode.log" hdfs&
 sleep 15
 
 # 4 init basic hdfs directories
@@ -18,15 +21,19 @@ sleep 15
 # su -s /bin/bash hdfs -c '/opt/hadoop/bin/hadoop fs -mkdir /tmp/hadoop-yarn/staging/history && /usr/bin/hadoop fs -chown mapred:mapred /tmp/hadoop-yarn/staging/history && /usr/bin/hadoop fs -chmod -R 1777 /tmp/hadoop-yarn/staging/history'
 
 # 5 init hive directories
-# su -s /bin/bash hdfs -c '/opt/hadoop/bin/hadoop fs -mkdir -p /user/hive/warehouse'
+# su -s /bin/bash hdfs -c '/opt/hadoop/bin/hadoop fs -mkdir /user/hive/warehouse'
 # su -s /bin/bash hdfs -c '/opt/hadoop/bin/hadoop fs -chmod 1777 /user/hive/warehouse'
 # su -s /bin/bash hdfs -c '/opt/hadoop/bin/hadoop fs -chown hive /user/hive/warehouse'
+
+cp /usr/share/java/mysql-connector-java.jar $HIVE_HOME/lib/
+
+cp /etc/hive/conf/hive-site.xml /opt/hive/conf
+
 mkdir -p /var/log/hive
 chown hdfs:hdfs /var/log/hive
 
 mkdir -p /var/log/hadoop-yarn
 chown hdfs:hdfs /var/log/hadoop-yarn
-
 # 6 stop hdfs
 
 # 7 setup metastore
@@ -34,11 +41,14 @@ mysqld --initialize
 
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 
-/usr/sbin/mysqld --user=mysql --socket=/var/lib/mysql/mysql.sock --skip-networking=0 --skip-grant-tables &
+/usr/sbin/mysqld --user=mysql --skip-networking=0 &
 sleep 10s
 
+MYSQL_PASS=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+
 cd /opt/hive/scripts/metastore/upgrade/mysql/
-echo "CREATE DATABASE metastore; USE metastore; SOURCE hive-schema-1.2.0.mysql.sql;" | mysql
+/usr/bin/mysqladmin --user=root --password="$MYSQL_PASS" password 'root'
+echo "CREATE DATABASE metastore; USE metastore; SOURCE hive-schema-4.0.0.mysql.sql;" | mysql -u root -proot
 
 pkill mysqld
 sleep 10s
